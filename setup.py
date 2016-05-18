4e2f9268dcca493e43a729ef933c176af94f1ce2
raleng <github@ralfengbers.com>
2016-05-18 14:20:25 +0200

    change comment

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
+# to build do:
+# python setup.py build_ext --inplace
+
 from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize("mur_funcs.pyx"),
 )
-
-# to build do:
-# python setup.py build_ext --inplace
