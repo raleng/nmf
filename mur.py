4928dca881774e1bdd8af052cbea484efa8fddaa
raleng <github@ralfengbers.com>
2016-09-07 14:12:29 +0200

    mostly PEP8 and other style fixes

--- mur.py
+++ mur.py
@@ -6,8 +6,8 @@ import scipy.io as sio
 import mur_funcs
 from misc import loadme
 
-def mur( X, k, kl=False, maxiter=100000, alpha_W=0, alpha_H=0,
-        dir_str="./results/", file_str="nmf_default" ):
+def mur(X, k, *, kl=False, maxiter=100000, alpha_W=0, alpha_H=0,
+        dir_str="./results/", file_str="nmf_default"):
     """ NMF with MUR
 
     Expects following arguments:
@@ -17,7 +17,7 @@ def mur( X, k, kl=False, maxiter=100000, alpha_W=0, alpha_H=0,
     #Parameters
     tol = 1e-3
     s = 1e-3
-    savestr = dir_str + file_str + '_' + str(k) + '_' + str(kl)
+    savestr = dir_str + file_str + '_' + str(k) + '_' + ('KL' if kl else 'EU')
     #savestr = './results/nmf_mur_' + str(k) + '_' + str(kl)
     #savestr = './results/delme2'
     if np.min(X) < 0:
@@ -29,8 +29,8 @@ def mur( X, k, kl=False, maxiter=100000, alpha_W=0, alpha_H=0,
     xdim = X.shape[0]
     samples = X.shape[1]
 
-    W = np.abs(np.random.randn(xdim,k))
-    H = np.abs(np.random.randn(k,samples))
+    W = np.abs(np.random.randn(xdim, k))
+    H = np.abs(np.random.randn(k, samples))
     #print('Loading initial matrices.')
     #inimat = sio.loadmat('/home/ralf/uni/data/msot-matlab/k4_ini.mat')
     #W = inimat['W_ini']
@@ -39,33 +39,33 @@ def mur( X, k, kl=False, maxiter=100000, alpha_W=0, alpha_H=0,
     WdotH = np.dot(W,H)
     if kl:
         print('Using Kullback-Leibler divergence.')
-        objhistory = [mur.funcs.dist_kl(X,WdotH)]
+        objhistory = [mur.funcs.dist_kl(X, WdotH)]
     else:
         print('Using euclidian distance.')
         #objhistory = [dist_euclid(X,WdotH)]
-        objhistory = [mur_funcs.dist_euclid(X,WdotH)]
+        objhistory = [mur_funcs.dist_euclid(X, WdotH)]
 
     for i in range(maxiter):
         begobj = objhistory[-1]
 
         if (i==maxiter-1):
-            np.savez(savestr,W=W,H=H,i=i,objhistory=objhistory)
+            np.savez(savestr, W=W, H=H, i=i, objhistory=objhistory)
             print('Max iteration. Results saved in ' + savestr)
 
         if kl:
-            W, H = mur.funcs.WH_update_kl( X, W, H )
+            W, H = mur_funcs.WH_update_kl(X, W, H)
         else:
-            W, H = mur_funcs.WH_update_euclid( X, W, H, alpha_W, alpha_H )
+            W, H = mur_funcs.WH_update_euclid(X, W, H, alpha_W, alpha_H)
 
-        norms = np.sqrt(np.sum(H.T**2,0))
-        H = H/norms[:,None]
+        norms = np.sqrt(np.sum(H.T**2, 0))
+        H = H/norms[:, None]
         W = W*norms
-        WdotH = np.dot(W,H)
+        WdotH = np.dot(W, H)
         if kl:
-            newobj = mur_funcs.dist_kl(X,W,H)
+            newobj = mur_funcs.dist_kl(X, W, H)
         else:
             #newobj = dist_euclid(X,WdotH)
-            newobj = mur_funcs.dist_euclid(X,WdotH)
+            newobj = mur_funcs.dist_euclid(X, WdotH)
 
         print('[' + str(i) + ']: ' + str(newobj))
         objhistory.append(newobj)
@@ -73,18 +73,19 @@ def mur( X, k, kl=False, maxiter=100000, alpha_W=0, alpha_H=0,
         #Konvergenzkriterium 1
         if newobj < tol:
             print('Algorithmus konvergiert (1)')
-            np.savez(savestr,W=W,H=H,i=i,objhistory=objhistory)
+            np.savez(savestr, W=W, H=H, i=i, objhistory=objhistory)
             print('Results saved in ' + savestr)
             break
 
         #Konvergenzkriterium 2
         if newobj >= begobj-s:
             print('Algorithmus konvergiert (2)')
-            np.savez(savestr,W=W,H=H,i=i,objhistory=objhistory)
+            np.savez(savestr, W=W, H=H, i=i, objhistory=objhistory)
             print('Results saved in ' + savestr)
             break
 
         if i%100 == 0:
+            np.savez(savestr, W=W, H=H, i=i, objhistory=objhistory)
             print('Saved on iteration ' + str(i) + ' in ' + savestr)
 
 if __name__ == '__main__':
@@ -92,10 +93,12 @@ if __name__ == '__main__':
 
     #Parsing arguments
     p = argparse.ArgumentParser()
-    #File loading MSOT
+    
+    #File loading MSOT (don't use -v)
     p.add_argument('-f', default='', type=str, dest='file_name')
-    #File loading MAT
+    #File loading MAT (use -f AND -v)
     p.add_argument('-v', default='LOAD_MSOT', type=str, dest='var_name')
+
     p.add_argument('-k', default=1, type=int)
     p.add_argument('-kl', action='store_true')
     p.add_argument('-i', default=100000, type=int, dest='maxiter')
@@ -120,5 +123,5 @@ if __name__ == '__main__':
             print('Data was 3D. Reshaped to 2D.')
 
     # call function
-    mur( X, k, kl=args.kl, maxiter=args.maxiter, alpha_W=args.alpha_W,
+    mur(X, k, kl=args.kl, maxiter=args.maxiter, alpha_W=args.alpha_W,
         alpha_H=args.alpha_H, file_str=args.file_str, dir_str=args.dir_str)
