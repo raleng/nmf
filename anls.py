bf91faa667736f2984738a427e08d3e497cd78f2
raleng <github@ralfengbers.com>
2017-07-21 18:29:42 +0200

    added stopping criteria

--- anls.py
+++ anls.py
@@ -1,16 +1,19 @@
+# system imports
+import begin
 import os
 from importlib import import_module
-
-import begin
 # noinspection PyUnresolvedReferences
 # import better_exceptions
-import numpy as np
+
+# math imports
 from math import sqrt
-from misc import loadme
+import numpy as np
 from scipy import optimize
 from sklearn.preprocessing import normalize
 
+# personal imports
 import fcnnls
+from misc import loadme
 
 
 def distance(x, wh):
@@ -54,6 +57,33 @@ def h_update(x, w, lambda_h, *, use_fcnnls=False):
     return h
 
 
+def gradient(x, w, h, lambda_w, lambda_h):
+    """ calculate gradient of W and H """
+    grad_w = w @ (h @ h.T) - x @ h.T + lambda_w * w
+    grad_h = (w.T @ w) @ h - w.T @ x + lambda_h * h
+    return grad_w, grad_h
+
+
+def init_stop_criterium(x, w, h, lambda_w, lambda_h):
+    """ """
+    grad_w, grad_h = gradient(x, w, h, lambda_w, lambda_h)
+    num_all = x.shape[0] * w.shape[1] + x.shape[1] * w.shape[1]
+    value = np.linalg.norm(np.concatenate(grad_w, grad_h.T), 'fro')/num_all
+    return value
+
+
+def stop_criterium(x, w, h, lambda_w, lambda_h):
+    """ calculate relative projected gradient """
+    grad_w, grad_h = gradient(x, w, h, lambda_w, lambda_h)
+
+    p_grad_w = grad_w[np.logical_or(grad_w < 0, w > 0)]
+    p_grad_h = grad_h[np.logical_or(grad_h < 0, h > 0)]
+    p_grad = np.append(p_grad_w, p_grad_h)
+    p_grad_norm = np.linalg.norm(p_grad)
+    value = p_grad_norm/p_grad.size
+    return value
+
+
 def convergence_check(new, old, tol1, tol2):
     """ Checks the convergence criteria """
 
@@ -69,7 +99,22 @@ def convergence_check(new, old, tol1, tol2):
     return convergence_break
 
 
+def convergence_sc(sc, sc_init, tol1):
+    convergence_break = True
+
+    rel_sc = sc/sc_init
+
+    if rel_sc <= tol1:
+        print('Algorithm converged: {}'.format(rel_sc))
+    else:
+        convergence_break = False
+
+    return convergence_break
+
+
 def save_results(convergence, max_iter, i, save_str, w, h, obj_history, experiment_dict):
+    """ save results """
+
     # Check convergence; save and break iteration
     if convergence and i > 10:
         np.savez(save_str, w=w, h=h, i=i, obj_history=obj_history,
@@ -113,31 +158,39 @@ def anls(x, k, *, use_fcnnls=False, lambda_w=0, lambda_h=0, max_iter=1000, tol1=
     tol = min(tol1, tol2)
     tol_precision = int(format(tol, 'e').split('-')[1]) if tol < 1 else 2
 
-    h = np.abs(np.random.randn(k, x.shape[1]))
-    h, norm = normalize(h, return_norm=True)
+    w = np.random.rand(x.shape[0], k)
+    h = np.random.rand(k, x.shape[1])
+
+    sc_init = stop_criterium(x, w, h, lambda_w, lambda_h)
 
     obj_history = [1e10]
 
+    # MAIN ITERATION
     for i in range(max_iter):
 
         # Update step
         w = w_update(x, h, lambda_w, use_fcnnls=use_fcnnls)
-        w = w * norm
-
         h = h_update(x, w, lambda_h, use_fcnnls=use_fcnnls)
-        h, norm = normalize(h, return_norm=True)
+
+        # h, norm = normalize(h, return_norm=True)
+        # w = w * norm
 
         # w, norm = normalize(w, axis=0, return_norm=True)
         # h = (h.T * norm).T
 
-        new_obj = distance(x, w@h)
+        obj_history.append(distance(x, w@h))
 
         # Iteration info
-        print('[{}]: {:.{}f}'.format(i, new_obj, tol_precision))
-        obj_history.append(new_obj)
+        sc = stop_criterium(x, w, h, lambda_w, lambda_h)
+
+        print('[{}]: {:.{}f} | {:.{}f}'.format(i, obj_history[-1], tol_precision,
+                                               sc/sc_init, tol_precision))
+
 
         # check convergence and save
-        converged = convergence_check(new_obj, obj_history[-2], tol1, tol2)
+        # converged = convergence_check(obj_history[-1], obj_history[-2], tol1, tol2)
+
+        converged = convergence_sc(sc, sc_init, tol1)
         if save_results(converged, max_iter, i, save_str, w, h, obj_history, experiment_dict):
             break
 
