5dda81a1ddf017daa3ae263f4c6aa377af746f75
raleng <github@ralfengbers.com>
2017-10-02 16:42:18 +0200

    anls.py

--- anls.py
+++ anls.py
@@ -57,31 +57,31 @@ def h_update(x, w, lambda_h, *, use_fcnnls=False):
     return h
 
 
-def gradient(x, w, h, lambda_w, lambda_h):
-    """ calculate gradient of W and H """
-    grad_w = w @ (h @ h.T) - x @ h.T + lambda_w * w
-    grad_h = (w.T @ w) @ h - w.T @ x + lambda_h * h
-    return grad_w, grad_h
+# def gradient(x, w, h, lambda_w, lambda_h):
+#     """ calculate gradient of W and H """
+#     grad_w = w @ (h @ h.T) - x @ h.T + lambda_w * w
+#     grad_h = (w.T @ w) @ h - w.T @ x + lambda_h * h
+#     return grad_w, grad_h
 
 
-def init_stop_criterium(x, w, h, lambda_w, lambda_h):
-    """ """
-    grad_w, grad_h = gradient(x, w, h, lambda_w, lambda_h)
-    num_all = x.shape[0] * w.shape[1] + x.shape[1] * w.shape[1]
-    value = np.linalg.norm(np.concatenate(grad_w, grad_h.T), 'fro')/num_all
-    return value
+# def init_stop_criterium(x, w, h, lambda_w, lambda_h):
+#     """ """
+#     grad_w, grad_h = gradient(x, w, h, lambda_w, lambda_h)
+#     num_all = x.shape[0] * w.shape[1] + x.shape[1] * w.shape[1]
+#     value = np.linalg.norm(np.concatenate(grad_w, grad_h.T), 'fro')/num_all
+#     return value
 
 
-def stop_criterium(x, w, h, lambda_w, lambda_h):
-    """ calculate relative projected gradient """
-    grad_w, grad_h = gradient(x, w, h, lambda_w, lambda_h)
-
-    p_grad_w = grad_w[np.logical_or(grad_w < 0, w > 0)]
-    p_grad_h = grad_h[np.logical_or(grad_h < 0, h > 0)]
-    p_grad = np.append(p_grad_w, p_grad_h)
-    p_grad_norm = np.linalg.norm(p_grad)
-    value = p_grad_norm/p_grad.size
-    return value
+# def stop_criterium(x, w, h, lambda_w, lambda_h):
+#     """ calculate relative projected gradient """
+#     grad_w, grad_h = gradient(x, w, h, lambda_w, lambda_h)
+#
+#     p_grad_w = grad_w[np.logical_or(grad_w < 0, w > 0)]
+#     p_grad_h = grad_h[np.logical_or(grad_h < 0, h > 0)]
+#     p_grad = np.append(p_grad_w, p_grad_h)
+#     p_grad_norm = np.linalg.norm(p_grad)
+#     value = p_grad_norm/p_grad.size
+#     return value
 
 
 def convergence_check(new, old, tol1, tol2):
@@ -173,13 +173,17 @@ def anls(x, k, *, use_fcnnls=False, lambda_w=0, lambda_h=0, max_iter=1000, tol1=
         # h = (h.T * norm).T
 
         # Iteration info and convergence check
-        sc = stop_criterium(x, w, h, lambda_w, lambda_h)
-        obj_history.append(distance(x, w@h))
-        print('[{}]: {:.{}f} | {:.{}f}'.format(i, obj_history[-1], tol_precision,
-                                               sc/sc_init, tol_precision))
+
+        # sc = stop_criterium(x, w, h, lambda_w, lambda_h)
+        # obj_history.append(distance(x, w@h))
+        # print('[{}]: {:.{}f} | {:.{}f}'.format(i, obj_history[-1], tol_precision,
+        #                                       sc/sc_init, tol_precision))
         # converged = convergence_check(obj_history[-1], obj_history[-2], tol1, tol2)
+
+        obj_history.append(distance(x, w@h))
+        print('[{}]: {:.{}f}}'.format(i, obj_history[-1], tol_precision))
         if i > 10:
-            converged = convergence_sc(sc, sc_init, tol1)
+            converged = convergence_check(obj_history[-1], obj_history[-2], tol1, tol2)
             if converged:
                 save_results(save_str, w, h, i, obj_history, experiment_dict)
                 print('Converged.')
@@ -217,7 +221,6 @@ def main(param_file='parameters_anls'):
     if data.ndim == 3:
         data = np.reshape(data, (data.shape[0]*data.shape[1], data.shape[2]), order='F')
         print('Data was 3D. Reshaped to 2D.')
-
 
     if params.use_fcnnls:
         print('Using FCNNLS.')
